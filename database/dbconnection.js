6ccf3fefe4a8dcd5498002345e163bac2ba88adb
ahmed mohamed <ahmed@emamil.com>
2026-02-14 21:13:48 +0200

    update

--- database/dbconnection.js
+++ database/dbconnection.js
@@ -1,7 +1,5 @@
 import mongoose from "mongoose";
 
 export const dbConnection = () => {
-  mongoose.connect(
-    "mongodb+srv://ahmed:ahmed123@cluster0.0l907kc.mongodb.net/E-Commerce",
-  );
+  mongoose.connect(process.env.DB_CONNECTION);
 };
